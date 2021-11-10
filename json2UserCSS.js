7282ce8856d4bf070a9123088c1be79e8919c487
kweilip <kweilip@gmail.com>
2021-11-10 19:26:34 +0800

    Add glob-promise to get all the stylus-*.json in the current folder

--- json2UserCSS.js
+++ json2UserCSS.js
@@ -2,6 +2,7 @@ const fs = require('fs-extra')
 const path = require('path')
 const sanitize = require('sanitize-filename')
 const css = require('css')
+const glob = require('glob-promise')
 
 const {compileMetadata} = require('./src/compileMetadata')
 const {compileRules} = require('./src/compileRules')
@@ -9,24 +10,31 @@ const {writeUserCSS} = require('./src/writeUserCSS')
 
 async function json2UserCSS() {
   const pConversion = []
-  const jsonStyles = fs.readJsonSync(`./stylus-backup.json`, {encoding: 'utf8'})
+  // get all .json file in the current folder
+  const jsonFiles = await glob('stylus-*.json', {cwd: process.cwd(), absolute: true})
+  let totalSectionFound = 0
 
-  for (const style of jsonStyles) {
-    const name = style['name']
+  for (const jsonFile of jsonFiles) {
+    const jsonStyles = fs.readJsonSync(jsonFile, {encoding: 'utf8'})
+    totalSectionFound += jsonStyles.length
 
-    if (name) {
-      for (const section of style['sections']) {
-        // populate metadata, rules, css contents with info from json
-        const filePath = `${sanitize(name)}.user.css`
+    for (const style of jsonStyles) {
+      const name = style['name']
 
-        const header = compileMetadata(name, filePath)
+      if (name) {
+        for (const section of style['sections']) {
+          // populate metadata, rules, css contents with info from json
+          const filePath = `${sanitize(name)}.user.css`
 
-        const rules = compileRules(section['urls'], section['urlPrefixes'], section['domains'], section['regexps'])
+          const header = compileMetadata(name, filePath)
 
-        const code = css.parse(section['code'])
-        const cssContent = css.stringify(code)
+          const rules = compileRules(section['urls'], section['urlPrefixes'], section['domains'], section['regexps'])
 
-        pConversion.push(writeUserCSS(header, rules, cssContent, filePath))
+          const code = css.parse(section['code'])
+          const cssContent = css.stringify(code)
+
+          pConversion.push(writeUserCSS(header, rules, cssContent, filePath))
+        }
       }
     }
   }
@@ -36,7 +44,7 @@ async function json2UserCSS() {
     // check how many promise returned true
     const successCount = results.filter(result => result).length
 
-    console.log(` ${successCount} out of ${jsonStyles.length} converted successfully`)
+    console.log(` ${successCount} out of ${totalSectionFound} converted successfully`)
   })
 }
 
